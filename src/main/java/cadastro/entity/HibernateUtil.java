5557b143dd006ced2f33f2f9f32cb3d6660bd1b2
Luiz Domingues <eldomingues@gmail.com>
2018-08-03 18:44:20 -0300

    Primeiro Commit

--- src/main/java/cadastro/entity/HibernateUtil.java
+++ src/main/java/cadastro/entity/HibernateUtil.java
@@ -1,42 +1,23 @@
 package cadastro.entity;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+
 
 public class HibernateUtil {
 
-//		private static SessionFactory sessionFactory = null;
-	    private static SessionFactory sessionFactory;
-		private static Configuration conf;
-		
-//		private static SessionFactory buildSessionFactory() {
-		static SessionFactory buildSessionFactory() {			
+	private static SessionFactory sessionFactory;
+
+	static SessionFactory buildSessionFactory() {			
 		try	{
-			
-			conf = new Configuration();
-			conf.configure("hibernate.cfg.xml");
-			System.out.println("Configurou");
-			
-			ServiceRegistry serviceRegistry;
-	        serviceRegistry = new StandardServiceRegistryBuilder()
-	                                 .applySettings(conf.getProperties())
-	                                 .build();
-	        System.out.println("Hibernate serviceRegistry created");
-	        
-	        sessionFactory = conf.buildSessionFactory(serviceRegistry);			
-//			sessionFactory = conf.buildSessionFactory();
+			sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 			System.out.println("Constroi");
-			
 			return sessionFactory;
-			
 		}catch (Throwable ex){
-			System.out.println("Erro na Conexão:  ");
+			System.out.println("Erro na Conexao:  ");
 			ex.printStackTrace();
 			throw new ExceptionInInitializerError(ex);
 		}
-//		return sessionFactory;
 	}
 		
 	public static SessionFactory getSessionFactory(){
